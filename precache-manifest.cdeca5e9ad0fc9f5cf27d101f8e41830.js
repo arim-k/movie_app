185951663d290f9c967f9cfb8c687ae3ac64fb49
splashwave <arim721@gmail.com>
2019-01-14 23:24:53 +0900

    Updates

---
+++ precache-manifest.cdeca5e9ad0fc9f5cf27d101f8e41830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "34cb75a8070ddadf8cd0",
+    "url": "/movie_app/static/js/main.34cb75a8.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "34cb75a8070ddadf8cd0",
+    "url": "/movie_app/static/css/main.dee01676.chunk.css"
+  },
+  {
+    "revision": "01c6e6f51728b688b523cf41be3d7e35",
+    "url": "/movie_app/index.html"
+  }
+];
